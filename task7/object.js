ace41843ab6e4d07c20cdc90fa6c69241cc55bd8
Haider-Ali127 <haidet0000@gmail.com>
2025-08-04 21:31:24 +0500

    task7

---
+++ task7/object.js
@@ -0,0 +1,69 @@
+// QUESTION 1 for practise
+const car = {
+    name: "civic",
+    id: 78,
+    model: 2020,
+    cc: 1600,
+    
+}
+for (let key in car) {
+  console.log(key, car[key]);
+} 
+// Acess, update, add, delete object property
+car.name = "grande";  // to update str
+car["model"] = 2022; // to update integer
+car.color = "blue",  // to add new property in your object
+delete car.id;   // to delet any property from your object
+console.log(car);
+
+// QUESTION 2 for practise
+const user = {
+   name:"haider",
+   address:{
+    city: "karachi",
+    area:"defence phase 7",
+    flatno: "A14",
+
+   }
+  }
+console.log("Haider Ali address: ")
+console.log(user.address.area + (" lane no.16 Bukhari commercial"));  
+console.log(("flat.no ") + user.address.flatno);
+console.log(user.address.city);// "Karachi"
+
+// QUESTION 3 FOR PRACTISE
+
+const fruits = {
+    name: "fruits",
+    Array : ["banana", "apple", "mango"],
+}
+ fruits.Array.push("peach"); // to add data at last in the array
+ fruits.Array.shift();  // to delete first data in array
+ fruits.Array.unshift("guava,kiwi"); // add data in first in the array.
+console.log(fruits.Array);
+
+// QUESTION 4 FOR PRACTISE
+ const person = {
+  username: "haider123",
+  email: "haider@example.com",
+  isActive: true
+ 
+};
+console.log( person);
+
+// lead code question solution
+
+function charfrequency(str) {
+     const frecounter = {};
+     for(let char of str){
+        if(frecounter[char]) {
+         frecounter[char] +=1;   
+        } else {
+            frecounter[ char] = 1;
+        }
+     }
+     return frecounter;
+    }     
+console.log(charfrequency("Haider Hassan Adnan"));
+
+
