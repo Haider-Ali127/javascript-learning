cc99f096484a9bfce36c0a4c93f7ad27cd4b1250
Haider-Ali127 <haidet0000@gmail.com>
2025-08-12 01:09:04 +0500

    dom manipulation

--- task11/dom.js
+++ task11/dom.js
@@ -1,20 +1,25 @@
-// DOM INTRO AND NAVIGATION 
+ // DOM MANUPULATION
+let tittle = document.getElementById("tittle");
 
-let heading = document.getElementById("tittle");
-console.log(heading.innerText);
+// now change the text
 
-let firstpara = document.querySelector(".text"); //dot  (.) to represent the class
-console.log(firstpara.innerText);
-
-let allparas = document.querySelectorAll(".text");
-console.log(allparas.length);   
+tittle.innerText = "New visible tittle"; //only visible text
+tittle.textContent ="New textcontent"; //inlcudes hidden tag
+tittle.innerHTML = " <em>Welcome to real world</em>"//can add html tags.
 
 
-let container = document.getElementById("container");
-console.log(container.parentNode);  
-console.log(container.children[0]); 
+let img = document.getElementById("logo");
+img.setAttribute("src", "https://tse3.mm.bing.net/th/id/OIP.gYug1yEyRSGuYm5FnRqj6gHaHa?pid=Api&P=0&h=220" );
+console.log(img.getAttribute("src"));
+let img2 = document.getElementById("logo2");
+img2.setAttribute("src", " https://tse2.mm.bing.net/th/id/OIP.t-NTmP7-zL4l8NwIhL-wdwHaHa?pid=Api&P=0&h=220" );
+console.log(img2.getAttribute("src"));
 
-let button = document.getElementById("tittle1");
-console.log(button.innerHTML);
+tittle.style.color = "black"; 
+tittle.style.fontSize = "50px";
 
-// DOM MANUPULATION
+tittle.classList.add("highlight"); // Adds CSS class
+tittle.classList.remove("highlight"); // Removes it
+tittle.classList.toggle("highlight");
+
+
