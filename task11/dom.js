a3c84da7b9f40e484a5681570e53e92135022691
Haider-Ali127 <haidet0000@gmail.com>
2025-08-12 01:16:49 +0500

    dom intro and navigation

--- task11/dom.js
+++ task11/dom.js
@@ -1,25 +1,20 @@
- // DOM MANiPULATION
-let tittle = document.getElementById("tittle");
+ // DOM INTRO AND NAVIGATION 
 
-// now change the text
+ let heading = document.getElementById("tittle");
+ console.log(heading.innerText);
 
-tittle.innerText = "New visible tittle"; //only visible text
-tittle.textContent ="New textcontent"; //inlcudes hidden tag
-tittle.innerHTML = " <em>Welcome to real world</em>"//can add html tags.
+ let firstpara = document.querySelector(".text"); //dot  (.) to represent the class
+ console.log(firstpara.innerText);
+
+ let allparas = document.querySelectorAll(".text");
+ console.log(allparas.length);   
 
 
-let img = document.getElementById("logo");
-img.setAttribute("src", "https://tse3.mm.bing.net/th/id/OIP.gYug1yEyRSGuYm5FnRqj6gHaHa?pid=Api&P=0&h=220" );
-console.log(img.getAttribute("src"));
-let img2 = document.getElementById("logo2");
-img2.setAttribute("src", " https://tse2.mm.bing.net/th/id/OIP.t-NTmP7-zL4l8NwIhL-wdwHaHa?pid=Api&P=0&h=220" );
-console.log(img2.getAttribute("src"));
+ let container = document.getElementById("container");
+ console.log(container.parentNode);  
+ console.log(container.children[0]); 
 
-tittle.style.color = "black"; 
-tittle.style.fontSize = "50px";
-
-tittle.classList.add("highlight"); // Adds CSS class
-tittle.classList.remove("highlight"); // Removes it
-tittle.classList.toggle("highlight");
+ let button = document.getElementById("tittle1");
+ console.log(button.innerHTML);
 
 
