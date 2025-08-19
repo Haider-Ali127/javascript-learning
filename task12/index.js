f8517357f2ec5a9bc38c3a37e43f49923c690ab4
Haider-Ali127 <haidet0000@gmail.com>
2025-08-20 03:17:33 +0500

    Task 12 completed

---
+++ task12/index.js
@@ -0,0 +1,18 @@
+ //---------MINI PROJECT TO DO LIST SLOUTION-------------//
+let btn = document.getElementById("addBtn")
+btn.addEventListener("click", () => {
+    let input = document.querySelector("Input");
+    if(!input.value.trim()) 
+        return;
+
+    let li = document.createElement("li");
+    li.innerHTML= `${input.value}  <button style="background-color: rgb(14, 18, 18); color:red;">DELETE</button>`;
+    li.querySelector("button").onclick = () => li.remove();
+
+    document.getElementById("taskList").appendChild(li);
+    input.value= "";
+});
+let btn1 = document.getElementById("btn1");
+btn1.addEventListener("dblclick", () => {
+      btn1.textContent =  alert(" TO DO LIST WAS SUBMITTED SUCCESSFULLY");
+});
