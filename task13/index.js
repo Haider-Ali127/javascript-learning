9f9828c175e80ba84cd84465b7cf3fec8450eb45
Haider-Ali127 <haidet0000@gmail.com>
2025-08-21 02:56:59 +0500

    task13 last leetcode soluton

--- task13/index.js
+++ task13/index.js
@@ -1,40 +1,12 @@
-//  ------ HOUR 4 ---------------------//
-// ------------spread rest operators--------//
-// expanding arrays
-let arr5 = [1, 2, 3];
-let arr6 = [4, 5, 6];
-let combined = [...arr5, ...arr6];
-console.log(combined);
-
-//spreading object to add pass new value and spread in array
-let pers = { name: "Haider", age: 19 };
-let updated = { ...pers, city: "karachi" };
-console.log(updated);
-
-// Rest operators--------
-let [first1, second, ...rest] = [10, 2, 30, 40, 50, 60];
-console.log(first1);
-console.log(second);
-console.log(rest);
-
-// rest with object destruction
-let obj = { id: 1, name: "haider", city: "karachi", age: 19 }
-let { id, ...details } = obj;
-console.log(`My id is ${id}`);
-console.log(details);
-
-
-//----- 4hours practise question------------------//
-let arr7 = [10, 20, 30]
-let arr8 = [40, 50, 60]
-let spread = [...arr7, ...arr8]
-console.log(spread);
-
-//-------Second question-----------//
-
-const sum = (...nums) => {
-    return nums.reduce((addall, n) => addall + n);
-
+//--------LEET CODE QUESTION-------------//
+function sum(num, target) {
+    for (let i = 0; i < num.length; i++) {
+        for (let j = 1; j < num.length; j++) {
+            if (num[i] + num[j] === target) {
+                return [i, j]
+            }
+        }
+    }
 }
-console.log(sum(10, 20, 30, 40, 50));
+console.log(sum([1, 2, 3, 4, 5, 6], 11));
 
