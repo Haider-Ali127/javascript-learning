b8f35e38207e09da48a090a6fef0ffad42bbe932
Haider-Ali127 <haidet0000@gmail.com>
2025-08-21 02:55:05 +0500

    arrdestruction and spreading

--- task13/index.js
+++ task13/index.js
@@ -1,16 +1,35 @@
-// -------2nd hour Topic ------------//
+//-----------3rd Hour started------------------------//
+// array destruction-----
+let arr1 = [2, 3, 4];
+let [a, b, c] = arr1;
+console.log(a);
+console.log(c);
+console.log(b);
 
-// multi-line string
+//-----Default values---------------//
+let ar = [7];
+let [num1, num2 = 100] = ar;
+console.log(num1);
+console.log(num2);
 
-let poem = `
-Roses are red
-flowers are red
-love is good
-`
-console.log(poem)
-//-------2nd Hour practise question----------------//
-function person2(name, age) {
-    console.log(`My name is ${name} and my age is ${age}`)
-}
-console.log(person2("Haider", 19));
+//---------swapping variablesz------------------//
+let h = 1; d = 2;
+[h, d] = [d, h];
+console.log(h);
+console.log(d);
+
+//-----------SKIPPING ARRAYS----------------//
+let arr4 = [100, 2, 350, 4];
+let [first, , third] = arr4;
+console.log(first); // 1
+console.log(third); // 3
+
+
+//---------3rd hour practise question--------------//
+let person1 = { name: "Haider", age: 19, city: "karachi" }
+let { name: age } = person1;
+let { age: name } = person1;
+console.log(`my age is ${age}`);
+console.log(`my name is ${name}`);
+
 
