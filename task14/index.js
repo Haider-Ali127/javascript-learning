135df36c3095279b982e07b24569df58d93dc48a
Haider-Ali127 <haidet0000@gmail.com>
2025-08-24 22:31:25 +0500

    task 14 leet code questions solved

--- task14/index.js
+++ task14/index.js
@@ -1,50 +1,47 @@
-function binary(nums, target) {
-    let start = 0;
-    let end = nums.length - 1
-
-    while (start <= end) {
-        let middle = Math.floor((start + end) / 2);
-
-        if (nums[middle] === target) {
-            return middle;
-        } else if (nums[middle] < target) {
-            start = middle + 1;
-        } else {
-            end = middle - 1;
+// ------leet code problem solution-----------------//
+function sum1(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            return i;
+        }
+          if (nums[i] > target) {
+            return nums[i] < target;
         }
     }
-    return -1
+    return -1;
 }
-console.log(binary([2, 4, 7, 10, 15], 10));
+console.log(sum1([1, 3, 5, 6], 5));  
+console.log(sum1([1, 3, 5, 7], 2));  
 
-// Time Complexity
+//-----------Question no.2 leetcode----------//
+function sum1(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            return i;
+        }
+    }
+    return -1;
+}
+console.log(sum1([1, 3, 5, 6], 6)); 
+console.log(sum1([1, 3, 5, 6], 7)); // this is for return -1
 
-// Best case  O(1) (target is exactly middle).
 
-// Worst case  O(log n).
+function sum3(nums, target) {
 
-// Average case  O(log n).
+    for (let i = 0; i < nums.length; i++) {
 
-//--------------linear search-------------------------//
-function sum(nums, target) {
-    for (let i = 0; nums.length; i++) {
         if (target === nums[i]) {
-            return i
+            return i;
+        }
+        else if (target < nums[i]) {
+            return i;
+        }
+        else if (target > nums[i]) {
+            nums.push(target);
         }
     }
     return -1
 }
-console.log(sum([1, 3, 5, 6], 5));
-
-// -------------Hour 4-----------------//
-
-// Time Complexity
-
-// Best case   O(1) (target at first element).
-
-// Worst case  O(n) (target at end or not found).
-
-// Average case  O(n/2) ≈ O(n).
-
-// Answer: Binary Search is better in efficiency when the data is sorted.
-// If data is unsorted  use Linear Search.
+console.log(sum3([1, 3, 5, 6], 5))
+console.log(sum3([1, 3, 5, 6], 2));
+console.log(sum3([1, 3, 5, 6], 7)); 
