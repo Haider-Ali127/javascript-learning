a7dc77eeefc3c97d982bb26f4a410e639a43b7f2
Haider-Ali127 <haidet0000@gmail.com>
2025-08-25 20:14:36 +0500

    task 15 practical ques no.1

--- task15/index1.js
+++ task15/index1.js
@@ -1,52 +1,16 @@
-// test scoping rules
+// --------Practical task--------------
 
-let glob = "global";
+let global = " i am global";
+
 function test() {
-    let local = "local"
-    console.log("outside fun: ", glob);
-    console.log("outside fun: ", local);
-}
-console.log(glob);
-// console.log(local);
-
-if (true) {
-    var x = 10;
-    let y = 20;
-    const z = 30;
-}
-// works with var not let and const
-console.log("x =", x);
-// console.log("y =", y);
-// console.log("z =", z);
-
-for (var i = 0; i < 3; i++) {
-    console.log(i);
-}
-console.log(i);
-
-// not work if change let with var it works
-for (var j = 0; j < 3; j++) {
-    console.log(j);
-}
-console.log(j);
-
-
-let a = "Global A";
-
-function outer() {
-    let b = "Outer B";
-
-    function inner() {
-        let c = "Inner C";
-        console.log("From inner:", a, b, c);
-        console.log("From inner:", c);
-    }
-
-    inner();
-    console.log(c)
-    console.log("From outer:", a, b, c);
-    // comes error c is not inner not accessible outside
-    console.log("From outer:", c)
+    let local = "i am local";
+    console.log(global);
+    console.log(local);
 }
 
-outer();
+test();
+console.log(global);
+// console.log(local); error undefined because you are trying to access a variable inside the function outside the scope. if you want to access it you want to invoke inside the scope of your function.
+
+
+
