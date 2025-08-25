d24e54c90fd8231fd9e8f2f2ba6e27fce426b75a
Haider-Ali127 <haidet0000@gmail.com>
2025-08-25 22:19:33 +0500

    task 15 last problems completed

--- task15/index1.js
+++ task15/index1.js
@@ -1,11 +1,41 @@
-// 4th question FUNCTION HOSTING 
-// this code will run in function dec it allows the code to access run before decleration
-sayhaider();
-function sayhaider() {
-    console.log("Haider");
+// last problems no.1
+for (var i = 0; i < 5; i++) {
+    console.log(i);
 }
-// but here function expression not allow to execute or invoke func before dec so this will give referenceerror
-sayhaider();
-let sayhaider = function () {
-    console.log("Haider");
+for (let j = 0; i < 5; i++) {
+    console.log(i);
+}
+for (const c = 0; i < 5; i++) {
+    console.log(i);
+}
+console.log(i) // runs only with var
+console.log(j) //using let giving error
+console.log(c) //using const giving error
+
+// no.2
+let global = 20;
+
+function local() {
+    let Local = 40;
+    console.log(global + Local)
+}
+local();
+
+// question.3
+// it is undefined means it is exist but undefined not give error
+console.log(a)
+var a = 10
+
+// let is also hoisted, but you cannot access before initialization it throws an error
+console.log(b)
+let b = 10
+// same thing using const throws an error crash your program
+console.log(h)
+const h = "haider"
+
+
+// this func will safely run not matter you trying to access befor func decleration 
+test();
+function test() {
+    console.log("Hello")
 }
