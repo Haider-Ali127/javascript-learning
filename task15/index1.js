3f748c46c66f7fa9a8484f252393c6eac0db2852
Haider-Ali127 <haidet0000@gmail.com>
2025-08-25 20:17:06 +0500

    practical.ques4 task 15

--- task15/index1.js
+++ task15/index1.js
@@ -1,10 +1,11 @@
-// 3rd practical question
-// using var it shows undefined but not gives error because it store in TDZ(TEMPORARY DEAD ZONE) and will hosited but undefined 
-console.log(a)
-var a = 5;
-// using let and const you cannot use before decleration the variable.
-console.log(b)
-let b = 10;
-// it will be accesseble after decleration
-console.log(b);
-
+// 4th question FUNCTION HOSTING 
+// this code will run in function dec it allows the code to access run before decleration
+sayhaider();
+function sayhaider() {
+    console.log("Haider");
+}
+// but here function expression not allow to execute or invoke func before dec so this will give referenceerror
+sayhaider();
+let sayhaider = function () {
+    console.log("Haider");
+}
