c6dfcab59d71bc92da0c70a9a19618e77a0a2fef
Haider-Ali127 <haidet0000@gmail.com>
2025-08-27 19:19:23 +0500

    task16 nested callback hell

--- task16/Index.js
+++ task16/Index.js
@@ -1,34 +1,21 @@
-
-// CALLBACKS IS A FUNCTION PASSED as a ARGUMENT TO ANOTHER FUNCTION.
-// IN callbacks you can pass the function as an argument and also passed the complete function its also valid syntax.
-// nested callback
-let age = 19;
-if (age >= 18) {
-    if (age >= 60) {
-        console.log("Senior")
-    } else {
-        console.log("middle child");
-    }
-} else {
-    console.log("child")
+function getData(dataId, getNextData, getanotherData) { // 2s
+    setTimeout(() => {
+        console.log("data", dataId);
+        if (getNextData) {
+            getNextData();
+        }
+    }, 2000)
 }
 
-
-// function sum(a, b) {
-//     console.log(a + b);
-// }
-
-function calculator(a, b, sumCallback) {
-    sumCallback(a, b);
-}
-// complete func passed. if write just sum in place of (a,b) so upper comment func runs.
-calculator(6, 6, (a, b) => {
-    console.log(a + b)
+// callback for second data
+// callback hell also called nested callback
+getData(1, () => {
+    console.log("getting data2......")
+    getData(2, () => {
+        console.log("getting 3......")
+        getData(3, () => {
+            console.log("getting data4......")
+            getData(4);
+        })
+    });
 })
-
-// one more eg of callback
-function num() {
-    console.log("10")
-}
-// num callbacks as an argument 
-setTimeout(num, 2000)
