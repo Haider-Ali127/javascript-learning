9ba1b744b2d2f31d8beb487be9551480f1fe0b74
Haider-Ali127 <haidet0000@gmail.com>
2025-08-28 01:38:30 +0500

    task16 async await

--- task16/Index.js
+++ task16/Index.js
@@ -1,41 +1,25 @@
-// PROMISES STARTED (states: pending, fulfilled, rejected).
-const getPromise = () => {
-    return new Promise((resolve, reject) => {
-        console.log("I am a promise");
-        // resolve("success");
-        reject("REJECTED")
-    })
-}
-let promise = getPromise();
-promise.then((res) => {
-    console.log("fullfiled successfully", res);
-})
-promise.catch((err) => {
-    console.log("NETWORK ERROR", err);
-});
+// ASYNC AWAIT
 
-// promise chaining
-function asyncfun1() {
+// async function hello() {
+//     console.log("hello");
+// }
+
+function api() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log("some data1");
-            resolve("Data fetched successfully....");
+            console.log("wheather api")
+            resolve(200)
         }, 4000)
     })
 }
-function asyncfun2() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("some data2");
-            resolve("Data fetched successfully....");
-        }, 4000)
-    })
-}
-console.log("fetching data1...")
-asyncfun1().then((res) => {
-    console.log(res)
-    console.log("fetching data2...")
-    asyncfun2().then((res) => {
-        console.log(res)
-    })
-})
+
+// async-await
+// to run code directly use iife use (and then use down async func and then add () in last)
+(async function getAlldata() {
+    console.log("fetching data1....")
+    await api(1);
+    console.log("fetching data2....")
+    await api(2);
+    console.log("fetching data3....")
+    await api(3);
+})()
