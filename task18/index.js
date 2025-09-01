8472ef912f9d55031f3227fb06daa883c1b5f237
Haider-Ali127 <haidet0000@gmail.com>
2025-09-01 18:06:24 +0500

    TASK18 QUES2 SOLVED

--- task18/index.js
+++ task18/index.js
@@ -1,5 +1,21 @@
-// Task 2 FETCH WITH PROMISES
+// Task 2 second part
 fetch("https://jsonplaceholder.typicode.com/posts")
     .then(response => response.json())
+    .then(data => {
+        console.log("first five alement of json", data.slice(0, 5))
+    })
+    .catch(error => console.error(error));
+
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+    .then(post => {
+        console.log(" post single id", post)
+    })
+    .catch(error => console.error(error));
+
+fetch("https://jsonplaceholder.typicode.com/wrongendpoint")
+    .then(response => response.json())
     .then(data => console.log(data))
-    .catch(error => console.error("ERROR OCCURED: ", error))
+    .catch(error => console.error("Handled Error", error.message));
+
