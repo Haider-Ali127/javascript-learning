80c29abef17bf7aca2d6e6d11de26824fc96ab68
Haider-Ali127 <haidet0000@gmail.com>
2025-09-01 22:30:53 +0500

    task18 task1 ques solved

--- task18/index.js
+++ task18/index.js
@@ -1,12 +1,41 @@
-// Topic 4 Async/Await with fetch
-async function getPosts() {
+// last tasks. ques no.1
+// converting first code eg into async await
+async function getdata() {
     try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
         const data = await response.json();
-        console.log(data.slice(0, 4));
+        console.log(data.slice(0, 4))
     } catch (error) {
-        console.log("Error Occured", error)
+        console.log("error occured", error)
+    }
+
+}
+getdata();
+
+
+// converting 2nd code eg into async await
+
+async function getinfo() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            body: JSON.stringify({
+                title: "foo",
+                body: "bar",
+                userId: 1
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+
+        })
+        const data = await response.json();
+        console.log(data)
+    } catch (error) {
+        console.log("error occured", error)
     }
 }
+getinfo();
 
-getPosts();
+
+
