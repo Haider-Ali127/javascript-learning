c27a76e25d125d0b255e7c61685557ce2ef4e524
Haider-Ali127 <haidet0000@gmail.com>
2025-09-04 02:21:29 +0500

    task20 last mini project completed

--- task20/index.js
+++ task20/index.js
@@ -1,18 +1,43 @@
-// practical exercise no.4
-class student {
-    constructor(grade) {
-        this.grade = grade
-    }
-    static compare(s1, s2) {
-        if (s1.grade > s2.grade) {
-            return ` student 1 get higher grade`
-        } else {
-            return `student 2 get higher grade`
-        }
+// task 20 last mini project
+class book {
+    constructor(title, author, year) {
+        this.title = title
+        this.author = author
+        this.year = year
     }
 }
-const student1 = new student(40)
-const student2 = new student(80)
+class library {
+    constructor() {
+        this.books = []
+    }
+    addBook(book) {
+        this.books.push(book)
+    }
+    removeBook(title) {
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i].title === title) {
+                this.books.splice(i, 1)
+                break
+            }
+        }
+    }
+    showBooks() {
+        this.books.forEach(book => {
+            console.log(`Title: ${book.title}, Author: ${book.author}, Year: ${book.year}`);
+        });
+    }
+}
+const book1 = new book("Harry Potter", "J.K. Rowling", 1997);
+const book2 = new book("The Hobbit", "J.R.R. Tolkien", 1937);
+const book3 = new book("1984", "George Orwell", 1949);
 
-console.log(student.compare(student1, student2))
-console.log("you got a grade congratulations")
+
+const myLibrary = new library();
+myLibrary.addBook(book1);
+myLibrary.addBook(book2);
+myLibrary.addBook(book3);
+console.log("All books in the library:");
+myLibrary.showBooks();
+myLibrary.removeBook("The Hobbit");
+console.log("\nBooks in the library after removing 'The Hobbit':");
+myLibrary.showBooks();
