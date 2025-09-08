394f3c7f66dbded4a2ca6d2e19b9544de70c460d
Haider-Ali127 <haidet0000@gmail.com>
2025-09-08 22:15:56 +0500

    task 20 static keyword

--- task21/Index.js
+++ task21/Index.js
@@ -1,19 +1,22 @@
+// A static method belongs to the class itself, not to an object (instance).
+// You call it using the class name, not an object.
+
+class mul {
+    static num(a, b) {
+        return a * b
+    }
+}
+console.log("THE ANS OF TWO NUM MUTIPLY IS:", mul.num(10, 20))
+
+// if you want to make an object using static method use in standered way
 class person {
     constructor(name, age) {
         this.name = name
         this.age = age
     }
-}
-class Age extends person {
-    constructor(name, age, id) {
-        super(name, age)
-        this.id = id
-    }
-    hello() {
-        console.log("My name is: ", this.name)
-        console.log("My age is :", this.age)
-        console.log("My id is: ", this.id)
+    static createper() {
+        return new person("Anonymus", 18)
     }
 }
-const age2 = new Age("Haider", 19, 1526);
-age2.hello()
+const p1 = person.createper();
+console.log("MY NAME IS : ", p1.name, + " " + "MY AGE IS:", p1.age)
