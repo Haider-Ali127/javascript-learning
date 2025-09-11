bba8b4f1b11aedd237b86101e638d0c4ca2fd02a
Haider-Ali127 <haidet0000@gmail.com>
2025-09-11 18:12:10 +0500

    task22 leet code problem no.1 solved

--- task22/inde.js
+++ task22/inde.js
@@ -1,23 +1,8 @@
-// practise task(prototype)
-function Shape(width, height) {
-    this.width = width;
-    this.height = height;
-
-
-    Shape.prototype.Area = function () {
-        console.log("Hello")
-    }
+// leet code problem No.1
+x = "12345"
+y = []
+for (let i = x.length; i >= 0; i--) {
+    y.push(x[i])
 }
+console.log(y.join(""))
 
-function Rectangle(width, height) {
-    Shape.call(this, width, height)
-}
-
-Rectangle.prototype = Object.create(Shape.prototype)
-Rectangle.prototype.constructor = Rectangle
-
-Rectangle.prototype.Area = function () {
-    console.log(this.width * this.height)
-}
-const r1 = new Rectangle(20, 6)
-r1.Area()
