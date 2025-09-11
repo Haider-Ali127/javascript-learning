7b11f390fc9b43aad33584633dfd554e34301963
Haider-Ali127 <haidet0000@gmail.com>
2025-09-11 15:48:31 +0500

    task22 prototype inheritence

--- task22/inde.js
+++ task22/inde.js
@@ -1,18 +1,93 @@
-// prototype A prototype is like a backup plan for objects.prototype is just another object that helps share properties and methods.
+// prototype inheritence: If an object doesn’t find a property or method, it looks into its prototype.This process is called prototype inheritance.
 
-function person(name) {
+let parent = {
+    canspeak: true,
+    speak() {
+        console.log("I can speak");
+    }
+};
+let child = {
+    canplay: true
+};
+child.__proto__ = parent;
+console.log(child.canplay);
+console.log(child.canspeak);
+child.speak()
+
+
+function Animal() { }
+Animal.prototype.walk = function () {
+    console.log("Animals walk just like a dog")
+};
+function Dog() { }
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.bark = function () {
+    console.log("Dog says WOOF WOOF!")
+}
+let d = new Dog();
+d.walk();
+d.bark();
+
+
+function Animal(name) {
     this.name = name
 }
-person.prototype.sayhello = function () {
-    console.log("HELLO MY NAME IS " + this.name)
+Animal.prototype.speak = function () {
+    console.log(this.name + " Makes a sound")
+};
+function dog(name) {
+    Animal.call(this, name)
 }
-const p1 = new person("haider")
-p1.sayhello()
-const p2 = new person("ali")
-p2.sayhello()
-const p3 = new person("hussain")
-p3.sayhello()
-const p4 = new person("ismail")
-p4.sayhello()
-const p5 = new person("ali")
-p5.sayhello()
+dog.prototype = Object.create(Animal.prototype)
+dog.prototype.constructor = dog;
+
+dog.prototype.speak = function () {
+    console.log(this.name + " barks");
+};
+
+const t = new Animal("Buddy")
+t.speak();
+
+
+
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.hello = function () {
+    console.log("HELLO, " + this.name);
+};
+
+function AgePerson(name, age) {
+
+    Person.call(this, name, age);
+}
+
+AgePerson.prototype = Object.create(Person.prototype);
+AgePerson.prototype.constructor = AgePerson;
+
+AgePerson.prototype.ageper = function () {
+    console.log(this.name + " is " + this.age + " years old");
+};
+
+const n = new AgePerson("Haider", 19);
+n.hello();
+n.ageper();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
